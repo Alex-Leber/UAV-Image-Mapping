1669a6bc9637a9194b079577a26af227e2d72bff
Trivago1 <al3xleber@outlook.com>
2026-05-15 03:08:09 -0400

    pure transformation homography implementation with test for approximating near nadir camera

---
+++ homography.py
@@ -0,0 +1,71 @@
+import numpy as np
+import cv2
+import calibration
+
+def inverse_rotation_homography(pose, K):
+    # pose = [x,y,z,Y,P,R] orientation from the IMU/GP drone metadata. Angles in radians
+    # K = camera intrinsics calculated from chessboard image in calibration.py
+    Y = pose[3] # yaw (z)
+    P = pose[4] # pitch (y)
+    R = pose[5] # roll (x)
+
+    Rx = np.array([[1,         0,            0],
+                   [0, np.cos(R), -1*np.sin(R)],
+                   [0, np.sin(R),    np.cos(R)]])
+    
+    Ry = np.array([[   np.cos(P), 0, np.sin(P)],
+                   [           0, 1,        0,],
+                   [-1*np.sin(P), 0, np.cos(P)]])
+    
+    Rz = np.array([[np.cos(Y), -1*np.sin(Y), 0],
+                   [np.sin(Y),    np.cos(Y), 0],
+                   [        0,            0, 1]])
+    
+    R = Rz @ Ry @ Rx # total rotation
+    K_inv = np.linalg.inv(K)
+    H = K @ R.T @ K_inv 
+    return H
+
+def unrotate_image(img, pose, K):
+
+    H = inverse_rotation_homography(pose, K)
+    h, w = img.shape[:2]
+    
+    corners = np.float32([[[0,0]], [[0,h]], [[w,h]], [[w,0]]])
+    transformed_corners = cv2.perspectiveTransform(corners, H)
+    xs = transformed_corners[:, 0, 0]
+    ys = transformed_corners[:, 0, 1]
+    x_min, y_min = xs.min(), ys.min()
+    x_max, y_max = xs.max(), ys.max()
+
+    # Translation of image in homogenous coordinates (adding third coordinate)
+    T = np.array([[1, 0, -x_min],
+                 [0, 1, -y_min],
+                 [0, 0, 1     ]])
+    
+    H_total = T @ H
+    new_w = int(x_max - x_min)
+    new_h = int(y_max - y_min)
+    img_rotated = cv2.warpPerspective(img, H_total, (new_w, new_h), flags=cv2.INTER_CUBIC)
+
+    return img_rotated
+
+def load_image(path):
+    img = cv2.imread(path)
+    if img is None:
+        raise FileNotFoundError(f"Couldn't load file {path}")
+    return img
+
+img = load_image("test1.jpg")
+K = calibration.calib_mtx()
+rotated_img = unrotate_image(img, [0,0,0,0,-0.1,-0.9], K)
+cv2.imshow("img", rotated_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+    
+    
+
+
